ce6c929816c3f303fc1e44bf8669fc18c63a47da
Jerson Moreno <jersonmr@gmail.com>
2025-08-31 17:35:41 -0400

    Actualiza la configuración de Starlight y la documentación de verificación de versión; elimina guías de ejemplo y agrega logo de Pisto.

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,26 +14,26 @@ export default defineConfig({
     },
     integrations: [
         starlight({
-            title: "My Docs",
-            social: [
-                {
-                    icon: "github",
-                    label: "GitHub",
-                    href: "https://github.com/withastro/starlight",
-                },
-            ],
+            title: "Pisto Docs",
+            // social: [
+            //     {
+            //         icon: "github",
+            //         label: "GitHub",
+            //         href: "https://github.com/withastro/starlight",
+            //     },
+            // ],
             sidebar: [
-                {
-                    label: "Guides",
-                    items: [
-                        // Each item here is one entry in the navigation menu.
-                        { label: "Example Guide", slug: "guides/example" },
-                    ],
-                },
-                {
-                    label: "Reference",
-                    autogenerate: { directory: "reference" },
-                },
+                // {
+                //     label: "Guides",
+                //     items: [
+                //         // Each item here is one entry in the navigation menu.
+                //         { label: "Example Guide", slug: "guides/example" },
+                //     ],
+                // },
+                // {
+                //     label: "Reference",
+                //     autogenerate: { directory: "reference" },
+                // },
                 {
                     label: "Verificación de versión",
                     autogenerate: { directory: "verificacion-de-version" },
